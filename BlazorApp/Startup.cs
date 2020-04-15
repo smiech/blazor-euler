ef00670e6e6228b3f7360e9d6bd04b550a93b881
Adam Smiechura <adam@72media.nl>
2020-04-15 20:26:30 +0200

    Remove example code

--- BlazorApp/Startup.cs
+++ BlazorApp/Startup.cs
@@ -29,7 +29,6 @@ namespace BlazorApp
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
             services.AddSingleton<SolutionService>();
         }
 
