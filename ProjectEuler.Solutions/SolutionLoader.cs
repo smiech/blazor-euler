7c4d448db0c96479e6076407d8edcd83e55ea806
adam.smiechura@gmail.com <sqx75cag53vPaia3TEKvMUnTE6Gv4J9E>
2021-09-14 21:18:09 +0200

    shotgun

--- ProjectEuler.Solutions/SolutionLoader.cs
+++ ProjectEuler.Solutions/SolutionLoader.cs
@@ -28,15 +28,7 @@ namespace ProjectEuler.Solutions
 
         static Assembly LoadPlugin(string relativePath)
         {
-            // Navigate up to the solution root
-            string root = Path.GetFullPath(Path.Combine(
-                Path.GetDirectoryName(
-                    Path.GetDirectoryName(
-                        Path.GetDirectoryName(
-                            Path.GetDirectoryName(
-                                Path.GetDirectoryName(typeof(SolutionLoader).Assembly.Location)))))));
-
-            string pluginLocation = Path.GetFullPath(Path.Combine(root, relativePath.Replace('\\', Path.DirectorySeparatorChar)));
+            string pluginLocation = Path.GetFullPath(relativePath.Replace('\\', Path.DirectorySeparatorChar));
             Console.WriteLine($"Loading commands from: {pluginLocation}");
             PluginLoadContext loadContext = new PluginLoadContext(pluginLocation);
             return loadContext.LoadFromAssemblyName(new AssemblyName(Path.GetFileNameWithoutExtension(pluginLocation)));
